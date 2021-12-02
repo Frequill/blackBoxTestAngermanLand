4a772111b2adcbb41efc22bf7597f3ef03ed32a9
Julius <julius.thomsen99@gmail.com>
2021-12-02 15:24:55 +0100

    Henning löste det (Döp om klasserna)

---
+++ Test/TestClass.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class TestClass {
+
+    @Test
+    void countVowelsTest(){
+        assertEquals (2,Iqh.countVowels("Toros"));
+    }
+
+}
